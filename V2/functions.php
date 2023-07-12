f7449fe8b339540655d350d5eb56d40ff92de661
meejle <inbox@mikelee.me.uk>
2023-07-12 18:51:06 +0100

    Initial commit

---
+++ V2/functions.php
@@ -0,0 +1,182 @@
+<?php
+
+/* Copyright © Critical Chicken. All rights reserved. Critical Chicken, the Critical Chicken logo and wordmark, and #ForTheGaymers are trademarks of Critical Chicken. All other trademarks referred to are trademarks of their respective owners. See website for current copyright years. */
+
+// Category link generator
+function categoryLinkGenerator() {
+
+	// Special categories, in order of priority
+	if (in_category('special')) {
+		if (in_category('best-of')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/best-of"><span class="titletag titletag-best-of">Best of</span></a>');
+		}
+		if (in_category('exclusives')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/exclusives"><span class="titletag titletag-yellow">Exclusive</span></a>');
+		}
+		if (in_category('live')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/live-updates"><span class="titletag titletag-live">Live</span></a>');
+		}
+		if (in_category('breaking-news')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/news"><span class="titletag titletag-yellow">Breaking news</span></a>');
+		}
+		if (in_category('news-alerts')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/news"><span class="titletag titletag-yellow">News alert</span></a>');
+		}
+		if (in_category('e3-2016')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/e3-2016"><span class="titletag titletag-e3">E3 2016</span></a>');
+		}
+		if (in_category('e3-2021')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/e3-2021"><span class="titletag titletag-e3">E3 2021</span></a>');
+		}
+		if (in_category('e3-2023')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/e3-2023"><span class="titletag titletag-e3">E3 2023 and beyond</span></a>');
+		}
+		if (in_category('summer-game-fest-2024')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/summer-game-fest-2024"><span class="titletag titletag-summer-game-fest">Summer Game Fest 2024</span></a>');
+		}
+		if (in_category('nintendo-direct')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/nintendo-direct"><span class="titletag titletag-nintendo-direct">Nintendo Direct</span></a>');
+		}
+		if (in_category('pokemon-presents')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/pokemon-presents"><span class="titletag titletag-pokemon-presents">Pok&eacute;mon Presents</span></a>');
+		}
+		if (in_category('state-of-play')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/state-of-play"><span class="titletag titletag-state-of-play">State of Play</span></a>');
+		}
+		if (in_category('ace-attorney')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/ace-attorney"><span class="titletag titletag-ace-attorney">Ace Attorney</span></a>');
+		}
+		if (in_category('forthegaymers')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/forthegaymers"><span class="titletag titletag-forthegaymers">#ForTheGaymers</span></a>');
+		}
+	}
+	
+	// News categories, in order of priority
+	if (in_category('news')) {
+		if (!in_category('breaking-news') && !in_category('news-alert')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/news"><span class="titletag titletag-news">News</span></a>');
+		}
+		if (in_category('updated')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/news"><span class="titletag titletag-hollow titletag-news">Updated</span></a>');
+		}
+		if (in_category('links')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/links"><span class="titletag titletag-hollow titletag-news">Link</span></a>');
+		}
+		if (in_category('live-updates')) {
+			if (!in_category('live')) {
+				echo('<a class="titletag-link" href="');
+				echo get_site_url();
+				echo('/category/live-updates"><span class="titletag titletag-hollow titletag-news">Live updates</span></a>');
+			}
+		}
+		if (in_category('rumours')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/rumours"><span class="titletag titletag-hollow titletag-news">Rumour</span></a>');
+		}
+		if (in_category('site-updates')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/site-updates"><span class="titletag titletag-hollow titletag-news">Site update</span></a>');
+		}
+		if (in_category('updates')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/updates"><span class="titletag titletag-hollow titletag-news">Update</span></a>');
+		}
+		if (in_category('videos')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/videos"><span class="titletag titletag-hollow titletag-news">Video</span></a>');
+		}
+	}
+	
+	// Feature categories, in order of priority
+	if (in_category('features')) {
+		echo('<a class="titletag-link" href="');
+		echo get_site_url();
+		echo('/category/features"><span class="titletag titletag-features">Feature</span></a>');
+		if (in_category('guest-blogs')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/guest-blogs"><span class="titletag titletag-hollow titletag-features">Guest blog</span></a>');
+		}
+		if (in_category('guides')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/guides"><span class="titletag titletag-hollow titletag-features">Guide</span></a>');
+		}
+		if (in_category('interviews')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/interviews"><span class="titletag titletag-hollow titletag-features">Interview</span></a>');
+		}
+		if (in_category('opinions')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/opinions"><span class="titletag titletag-hollow titletag-features">Opinion</span></a>');
+		}
+	}
+	
+	// Review categories, in order of priority
+	if (in_category('reviews')) {
+		echo('<a class="titletag-link" href="');
+		echo get_site_url();
+		echo('/category/reviews"><span class="titletag titletag-reviews">Review</span></a>');
+		if (in_category('first-impressions')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/first-impressions"><span class="titletag titletag-hollow titletag-reviews">First impressions</span></a>');
+		}
+		if (in_category('hands-on')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/hands-on"><span class="titletag titletag-hollow titletag-reviews">Hands on</span></a>');
+		}
+		if (in_category('previews')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/previews"><span class="titletag titletag-hollow titletag-reviews">Previews</span></a>');
+		}
+		if (in_category('second-looks')) {
+			echo('<a class="titletag-link" href="');
+			echo get_site_url();
+			echo('/category/second-looks"><span class="titletag titletag-hollow titletag-reviews">Second look</span></a>');
+		}
+	}
+
+}
---
+++ V2/js/scripts.js
@@ -0,0 +1,73 @@
+// JavaScript Document
+
+/* Copyright © Critical Chicken. All rights reserved. Critical Chicken, the Critical Chicken logo and wordmark, and #ForTheGaymers are trademarks of Critical Chicken.
+All other trademarks referred to are trademarks of their respective owners. See website for current copyright years. */
+
+// Animate the breaking news ticker
+$("#ticker-contents").webTicker({
+	height: "32px",
+	duplicate: true,
+	rssfrequency: 0,
+	startEmpty: true,
+	hoverpause: false,
+	speed: 96
+});
+
+
+// Display the page load time
+var beforeload = (new Date()).getTime();
+
+function getPageLoadTime(){
+
+	var r_text = new Array ();
+	r_text[0] = "built and paid for by Mexico";
+	r_text[1] = "coughed up by your nan’s cat";
+	r_text[2] = "performed by the London Symphony Orchestra";
+	r_text[3] = "coded in real time by one really stressed dude";
+	r_text[4] = "shot through a series of tubes";
+	r_text[5] = "dreamed up by a coma patient";
+	r_text[6] = "re-released in stunning high definition";
+	r_text[7] = "delivered by your DPD driver Chris";
+	r_text[8] = "passed down through the generations";
+	r_text[9] = "dredged from the Internet swamp";
+	r_text[10] = "scraped from the bottom of the barrel";
+	r_text[11] = "speedrun by a Japanese guy";
+	r_text[12] = "universally panned by critics";
+	r_text[13] = "written by renowned author Dan Brown";
+	r_text[14] = "hastily thrown together by GAME FREAK inc.";
+	r_text[15] = "copied and pasted off of Wikipedia";
+	r_text[16] = "whittled by a sexy lumberjack";
+	r_text[17] = "commissioned and cancelled by Netflix";
+	r_text[18] = "gifted, then immediately regifted";
+	r_text[19] = "fashioned from twigs and bits of string";
+	r_text[20] = "straightwashed by a historian";
+	r_text[21] = "hot glue-gunned to death by a suburban mom";
+	r_text[22] = "booed off stage at an open-mic night";
+	r_text[23] = "foretold by a biblically-accurate seraphim";
+	r_text[24] = "adapted into a major motion picture";
+	r_text[25] = "bought out and ruined by Elon Musk";
+
+	var i = Math.floor(r_text.length * Math.random());
+	var afterload = (new Date()).getTime();
+    seconds = (afterload-beforeload) / 1000;
+	if(seconds == 1) {
+		var plurality = ".";
+	} else {
+		var plurality = "s.";
+	}
+    $("#load_time").text('This page was ' + r_text[i] + ' in ' + seconds + ' second' + plurality);
+}
+
+// Fix pluralisation on the "waiting for Ace Attorney 7" clock
+$("span.timetip").each(function() {
+	if($(this).text().charAt(0) == "1" && $(this).text().charAt(1) == " ") {
+		$(this).text($(this).text().replace("years","year"));
+	}
+	$(this).text($(this).text().replace(" 1 months"," 1 month"));
+	$(this).text($(this).text().replace(" 1 days"," 1 day"));
+});
+
+// Handle functions that trigger when the page is fully loaded
+$(window).on("load", function() {
+	getPageLoadTime();
+});
