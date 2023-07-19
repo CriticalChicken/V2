2c81554aca4be7ccf8d23a5ff0ea01a38fda2bec
meejle <inbox@mikelee.me.uk>
2023-07-20 00:32:52 +0100

    Fixes and groundwork for listings pages.

--- V2/index.php
+++ V2/index.php
@@ -6,13 +6,24 @@ All other trademarks referred to are trademarks of their respective owners. See 
 
 <head>
 <?php get_template_part("V2-meta"); ?>
-<title>Critical Chicken - Video game news, features, and reviews #ForTheGaymers</title>
+<title>Our latest posts - Critical Chicken</title>
 <?php wp_head(); ?>
 </head>
 
 <body>
 <?php get_template_part("V2-header"); ?>
-
+<div id="listing-container">
+<h1>Our latest posts</h1>
+<div id="the-post-listing">
+<?php if(have_posts()):while(have_posts()):the_post(); ?>
+<div id="post-<?php the_ID(); ?>"<?php post_class('listing-post') ?>>
+<a href="<?php echo the_permalink(); ?>" title="Read more" class="thumb-link"><img src="<?php if(get_field('thumbnail')):echo the_field('thumbnail');else:echo get_template_directory_uri();fallbackThumbnailPicker();endif; ?>" alt="<?php if(get_field('alt')):echo the_field('alt');endif; ?>" width="352"></a>
+<?php categoryLinkGenerator(); ?>
+<span class="listing-post-title"><a href="<?php echo the_permalink(); ?>" title="Read more"><?php the_title(); ?></a></span>
+</div>
+<?php endwhile;endif; ?>
+</div><!-- /the-post-listing -->
+</div><!-- /listing-container -->
 <?php get_template_part("V2-footer"); ?>
 <?php wp_footer();?>
 </body>
