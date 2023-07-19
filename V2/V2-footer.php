4cc904f97b4cbb562fc11d36418e21fc79ea6180
meejle <inbox@mikelee.me.uk>
2023-07-20 00:01:28 +0100

    Various improvements including new low-resolution icons.

---
+++ V2/V2-footer.php
@@ -0,0 +1,23 @@
+<div id="footer-outer">
+<div id="footer-inner">
+<span class="copyright">Copyright &copy; 2016&ndash;<?php echo date("Y"); ?> Critical Chicken. All rights reserved.</span>
+<span class="trademark">Critical Chicken, the Critical Chicken logo and wordmark, and #ForTheGaymers are trademarks of Critical Chicken.<br>All other trademarks referred to are trademarks of their respective owners.</span>
+<div id="footer-links">
+<ul>
+<li><a href="<?php echo get_site_url(); ?>/privacy">Privacy policy &raquo;</a></li>
+<li><a href="<?php echo get_site_url(); ?>/roadmap">Roadmap &raquo;</a></li>
+<li><a href="<?php echo get_site_url(); ?>/promises">Our six promises &raquo;</a></li>
+</ul>
+</div><!-- /footer-links -->
+<div id="version">
+<img src="<?php echo get_template_directory_uri(); ?>/img/branding_v2_color-on-white_for32px.png" width="43" height="32" alt="Critical Chicken V2" title="V2 MVP - Brazen Blackbird"><p id="load_time">Still loading&hellip;</p>
+</div><!-- /version -->
+</div><!-- /footer-inner -->
+</div><!-- /footer-outer -->
+</div><!-- /container -->
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.4.1.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.webticker.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
+<script type="module" src="https://unpkg.com/friendly-challenge@0.9.5/widget.module.min.js" async defer></script>
+<script nomodule src="https://unpkg.com/friendly-challenge@0.9.5/widget.min.js" async defer></script>
+<!--<script data-host="https://microanalytics.io" data-dnt="false" src="https://microanalytics.io/js/script.js" id="ZwSg9rf6GA" async defer></script> UN-COMMENT THIS LINE BEFORE LAUNCH -->
