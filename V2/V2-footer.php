75b8bb98b16070f28dd0dfdf814b3fee3621463e
meejle <inbox@mikelee.me.uk>
2023-07-22 18:21:08 +0100

    Added socials and 'back to top' to footer. Added scrollgress.

--- V2/V2-footer.php
+++ V2/V2-footer.php
@@ -1,5 +1,15 @@
 <div id="footer-outer">
 <div id="footer-inner">
+<div id="footer-socials">
+<ul>
+<li class="footer-socials-discord"><a href="https://discord.gg/XtGUe4vaVm" title="Discord" target="_blank" rel="external me">Discord</a></li>
+<li class="footer-socials-instagram"><a href="https://www.instagram.com/criticalchicken" title="Instagram" target="_blank" rel="external me">Instagram</a></li>
+<li class="footer-socials-mastodon"><a href="https://ohai.social/@CriticalChicken" title="Mastodon" target="_blank" rel="external me">Mastodon</a></li>
+<li class="footer-socials-threads"><a href="<?php echo get_site_url(); ?>/threads" title="Threads" rel="help">Threads</a></li>
+<li class="footer-socials-ko-fi"><a href="https://ko-fi.com/criticalchicken" title="Ko-fi" target="_blank" rel="external me">Ko-fi</a></li>
+<li class="footer-socials-tumblr"><a href="#" title="Tumblr" target="_blank" rel="external me">Tumblr</a></li>
+</ul>
+</div>
 <span class="copyright">Copyright &copy; 2016&ndash;<?php echo date("Y"); ?> Critical Chicken. All rights reserved.</span>
 <span class="trademark">Critical Chicken, the Critical Chicken logo and wordmark, and #ForTheGaymers are trademarks of Critical Chicken.<br>All other trademarks referred to are trademarks of their respective owners.</span>
 <div id="footer-links">
@@ -9,6 +19,7 @@
 <li><a href="<?php echo get_site_url(); ?>/promises">Our six promises &raquo;</a></li>
 </ul>
 </div><!-- /footer-links -->
+<div id="back-to-top" title="Back to top"></div>
 <div id="version">
 <img src="<?php echo get_template_directory_uri(); ?>/img/branding_v2_color-on-white_for32px.png" width="43" height="32" alt="Critical Chicken V2" title="V2 MVP - Brazen Blackbird"><p id="load_time">Still loading&hellip;</p>
 </div><!-- /version -->
@@ -17,6 +28,7 @@
 </div><!-- /container -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.4.1.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.webticker.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/scrollgress.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js"></script>
 <script type="module" src="https://unpkg.com/friendly-challenge@0.9.5/widget.module.min.js" async defer></script>
 <script nomodule src="https://unpkg.com/friendly-challenge@0.9.5/widget.min.js" async defer></script>
