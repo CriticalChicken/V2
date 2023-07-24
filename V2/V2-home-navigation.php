91f686c8e67eecc21263ea31f72abca4269f7ee1
meejle <inbox@mikelee.me.uk>
2023-07-24 23:26:05 +0100

    Started laying the groundwork for smaller/mobile versions.

--- V2/V2-home-navigation.php
+++ V2/V2-home-navigation.php
@@ -4,6 +4,6 @@
 <li class="news"><a href="<?php echo get_site_url(); ?>/category/news">News</a></li>
 <li class="features"><a href="<?php echo get_site_url(); ?>/category/features">Features</a></li>
 <li class="reviews"><a href="<?php echo get_site_url(); ?>/category/reviews">Reviews</a></li>
-<li class="ko-fi"><a href="https://ko-fi.com/criticalchicken" target="_blank" rel="external me"><img src="<?php echo get_template_directory_uri(); ?>/img/ui_ko-fi_color-on-transparent_for16px.png" width="25" height="16" alt="">Support us on Ko-fi</a></li>
+<li class="ko-fi"><a href="https://ko-fi.com/criticalchicken" target="_blank" rel="external me"><img src="<?php echo get_template_directory_uri(); ?>/img/ui_ko-fi_color-on-transparent_for16px.png" width="25" height="16" alt=""><span>Support us</span></a></li>
 </ul>
 </div><!-- /home-navigation -->
