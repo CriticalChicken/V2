598f637d54a00530d3075b7e79c70edcf5122145
meejle <inbox@mikelee.me.uk>
2023-08-02 17:20:38 +0100

    Shaved off a few more unnecessary HTTP requests :)

--- V2/functions/maintenance-mode-editor.php
+++ V2/functions/maintenance-mode-editor.php
@@ -1,4 +1,4 @@
-<?php add_action('admin_menu','mm_editor');function mm_editor(){add_menu_page('Maintenance Mode','Maint. Mode','manage_options','mm-editor','mm_editor_contents','dashicons-hidden',3.6);}function mm_editor_contents(){ ?>
+<?php add_action('admin_menu','mm_editor');function mm_editor(){add_menu_page('Maintenance Mode','Maintenance Mode','manage_options','mm-editor','mm_editor_contents','dashicons-hidden',3.6);}function mm_editor_contents(){ ?>
 
 <div class="wrap">
 	<h1 style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/admin_v2_color-on-transparent_for31px.png'); background-size: 41.5px 31px; background-position: left 12px; background-repeat: no-repeat; padding: 8px 0 12px 53.5px !important; height: 31px !important; line-height: 31px !important;">Maintenance Mode</h1>
